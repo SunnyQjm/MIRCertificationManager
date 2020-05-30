1339a5d255759abf5e17a24017bc33bc5f640b18
SunnyQjm <qjm253@pku.edu.cn>
2020-05-30 20:00:29 +0800

    add: 完成安装卸载样例程序编写

---
+++ uninstallCertificate.cpp
@@ -0,0 +1,24 @@
+//
+// Created by mingj on 2020/5/30.
+//
+
+#include "utils/FileUtils.h"
+#include "utils/NDNCertificationUtil.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        cout << "Usage : ./installCert -f <filename> " << endl;
+    }
+
+    // 读取文件里的证书为字符串
+    string certStr = FileUtils::readFile2String(argv[2]);
+    cout << "Read cert: " << endl << certStr << endl;
+
+    NDNCertificationUtil ndnCertificationUtil;
+    ndnCertificationUtil.connect();
+    auto result = ndnCertificationUtil.uninstallCert(certStr);
+    cout << "install cert result: " << result << endl;
+}
